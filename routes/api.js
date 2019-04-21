d9e266232edf6216daca3c0450209ee36dc46bb7
Arty <26905074+artysidorenko@users.noreply.github.com>
2019-04-21 13:13:39 +0100

    Basic project completed

--- routes/api.js
+++ routes/api.js
@@ -9,15 +9,92 @@
 'use strict';
 
 var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+var request = require('request');
+const API_KEY = process.env.API_KEY;
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
-
-module.exports = function (app) {
+module.exports = function (app, collection) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
+      const stock = req.query.stock
+      const like = req.query.like
+      const ip = req.connection.remoteAddress
       
+      // OPTION 1 - REQUEST IS A SINGLE STOCK
+      if (!Array.isArray(stock)) {
+        // STEP 1 - check for likes in the database and add like if required
+        processLikes(stock, like, ip, (likes) => {
+          // STEP 2 - get external data from API
+          getExternalData(stock, (data) => {
+            // STEP 3 - form a bespoke response Obj with combined external and db data
+            res.json({
+              stockData: {
+                stock: data['01. symbol'],
+                price: data['05. price'],
+                likes: likes
+              }
+            })
+          })
+        })
+      }
+
+      // OPTION 2 - REQUEST IS AN ARRAY OF 2 STOCKS
+      else if (Array.isArray(stock)) {
+        console.log('option 2 triggered')
+        // STOCK 1 - LIKES
+        processLikes(stock[0], like, ip, (likes0) => {
+          // STOCK 1 - DATA
+          getExternalData(stock[0], (data0) => {
+            // STOCK 2 - LIKES
+            processLikes(stock[1], like, ip, (likes1) => {
+              //STOCK 2 - DATA
+              getExternalData(stock[1], (data1) => {
+                res.json({
+                  stockData: [
+                    {
+                      stock: data0['01. symbol'],
+                      price: data0['05. price'],
+                      rel_likes: likes0 - likes1
+                    },
+                    {
+                      stock: data1['01. symbol'],
+                      price: data1['05. price'],
+                      rel_likes: likes1 - likes0
+                    }
+                  ]
+                })
+              })
+            }) 
+          })
+        })
+      }
+
     });
+
+  function processLikes(stock, like, ip, callback) {
+    if (!like) {
+      collection.findOne({stock: stock}, (dbErr, doc) => {
+        if (dbErr) console.log(`Error fetching from mongodb: ${dbErr}`)
+        if (doc === null) callback(0)
+        else callback(doc.likes.length)
+      })
+    }
+    if (like) {
+      collection.findOneAndUpdate({stock: stock}, {$addToSet: {likes: ip}}, {returnOriginal: false, upsert: true}, (dbErr, doc) => {
+        if (dbErr) console.log(`Error fetching and updating mongodb: ${dbErr}`)
+        callback(doc.value.likes.length)
+      })
+    }
+  }
+
+  function getExternalData(stock, callback) {
+    const URL = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${API_KEY}`
+    request.get(URL, (requestErr, response, body) => {
+      if (requestErr) console.log(`Error fetching from external API: ${requestErr}`)
+      let json = JSON.parse(body)['Global Quote']
+      console.log(json)
+      callback(json)
+    })
+  }
     
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -13,8 +13,13 @@ var server = require('../server');
 
 chai.use(chaiHttp);
 
+// IMPORTANT!!!!
+// API ONLY EXCEPTS UP TO 5 REQUESTS PER MINUTE
+// CONSEQUENTLY IF ALL TESTS ARE RUN TOGETHER THE LAST ONE WILL FAIL
+// RUN INDIVIDUALLY/SPLIT INTO TWO GROUPS THEY WILL PASS
+
 suite('Functional Tests', function() {
-    
+
     suite('GET /api/stock-prices => stockData object', function() {
       
       test('1 stock', function(done) {
@@ -23,26 +28,86 @@ suite('Functional Tests', function() {
         .query({stock: 'goog'})
         .end(function(err, res){
           
-          //complete this one too
-          
+          assert.equal(res.status, 200, 'Response should be successful - 200')
+          assert.property(res.body.stockData, 'stock', 'response obj should have stock name')
+          assert.property(res.body.stockData, 'price', 'response obj should have stock price')
+          assert.property(res.body.stockData, 'likes', 'response obj should have stock likes count')
+          assert.equal(res.body.stockData.stock.toLowerCase(), 'goog', 'stock name should match data sent (case insensitive)')
           done();
         });
       });
       
+      let likes;
+
       test('1 stock with like', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({ stock: 'goog', like: true })
+          .end(function (err, res) {
+
+            assert.equal(res.status, 200, 'Response should be successful - 200')
+            assert.property(res.body.stockData, 'stock', 'response obj should have stock name')
+            assert.property(res.body.stockData, 'price', 'response obj should have stock price')
+            assert.property(res.body.stockData, 'likes', 'response obj should have stock likes count')
+            assert.equal(res.body.stockData.stock.toLowerCase(), 'goog', 'stock name should match data sent (case insensitive)')
+            assert.isAbove(res.body.stockData.likes, 0, 'stock has at least 1 like')
+            likes = res.body.stockData.likes
+            done();
+          });
       });
       
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({ stock: 'goog', like: true })
+          .end(function (err, res) {
+
+            assert.equal(res.status, 200, 'Response should be successful - 200')
+            assert.property(res.body.stockData, 'stock', 'response obj should have stock name')
+            assert.property(res.body.stockData, 'price', 'response obj should have stock price')
+            assert.property(res.body.stockData, 'likes', 'response obj should have stock likes count')
+            assert.equal(res.body.stockData.stock.toLowerCase(), 'goog', 'stock name should match data sent (case insensitive)')
+            assert.equal(res.body.stockData.likes, likes, 'stock has same likes as in previous request')
+            done();
+          });
       });
       
       test('2 stocks', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({ stock: ['goog', 'axp'] })
+          .end(function (err, res) {
+
+            assert.equal(res.status, 200, 'Response should be successful - 200')
+            assert.isArray(res.body.stockData, 'response should be an array')
+            assert.equal(res.body.stockData.length, 2, 'response array should have 2 entries')
+            assert.property(res.body.stockData[0], 'stock', 'response obj should have stock name')
+            assert.property(res.body.stockData[0], 'price', 'response obj should have stock price')
+            assert.property(res.body.stockData[0], 'rel_likes', 'response obj should have relatives likes count')
+            assert.equal(res.body.stockData[0].stock.toLowerCase(), 'goog', 'stock name should match data sent (case insensitive)')
+            assert.equal(res.body.stockData[0].rel_likes + res.body.stockData[1].rel_likes, 0, 'relative likes should cancel out')
+            done();
+          });
       });
       
       test('2 stocks with like', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({ stock: ['goog', 'axp'], like: true })
+          .end(function (err, res) {
+
+            assert.equal(res.status, 200, 'Response should be successful - 200')
+            assert.isArray(res.body.stockData, 'response should be an array')
+            assert.equal(res.body.stockData.length, 2, 'response array should have 2 entries')
+            assert.property(res.body.stockData[0], 'stock', 'response obj should have stock name')
+            assert.property(res.body.stockData[0], 'price', 'response obj should have stock price')
+            assert.property(res.body.stockData[0], 'rel_likes', 'response obj should have relatives likes count')
+            assert.oneOf(res.body.stockData[0].stock, ['GOOG', 'AXP']);
+            assert.oneOf(res.body.stockData[1].stock, ['GOOG', 'AXP']);
+            assert.equal(res.body.stockData[0].stock.toLowerCase(), 'goog', 'stock name should match data sent (case insensitive)')
+            assert.equal(res.body.stockData[0].rel_likes + res.body.stockData[1].rel_likes, 0, 'relative likes should cancel out')
+            done();
+          });
       });
       
     });
